d6ddacb257a5411efdaf3885e5869817679be801
Patryk Kwiatkowski <patrykkwiatkowski714@gmail.com>
2026-04-08 21:41:21 +0200

    waits and lambda

--- tests/e2e/test_e2e.py
+++ tests/e2e/test_e2e.py
@@ -19,14 +19,14 @@ def test_e2e_flow(driver):
     assert inventory.is_loaded()
 
     # Dodaj produkt
-    inventory.add_products(1)
-    assert inventory.get_cart_count() == "1"
+    inventory.add_products(3)
+    assert inventory.get_cart_count() == "3"
 
     # Przejdź do koszyka
     inventory.go_to_cart()
     print("URL AFTER CLICK:", driver.current_url)
     assert cart.is_loaded()
-    assert cart.get_items_count() == 1
+    assert cart.get_items_count() == "3"
 
     # Checkout
     cart.go_to_checkout()
