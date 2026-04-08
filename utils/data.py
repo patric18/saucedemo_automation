2a78f932ba0a6eb91582a01e472c83ac2587ddcd
Patryk Kwiatkowski <patrykkwiatkowski714@gmail.com>
2026-04-09 01:57:26 +0200

    testing checkout success and missing data - missing Error: in data

--- utils/data.py
+++ utils/data.py
@@ -25,7 +25,7 @@ INVALID_CHECKOUT = [
     ("","Test","00-001", "Error: First Name is required"),
     ("Jan","","00-001", "Error: Last Name is required"),
     ("Jan","Test","", "Error: Postal Code is required"),
-    ("","","", "Error: First Name is required")
+    ("","","", "Error: First Name is required"),
 ]
 
 VALID_CHECKOUT = ("Jan", "Test", "00-001")
