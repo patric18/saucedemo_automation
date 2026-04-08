15922d52c9d3e4fb674c022e6b0b288278bd8496
Patryk Kwiatkowski <patrykkwiatkowski714@gmail.com>
2026-04-08 15:00:16 +0200

    Added waits for Cart and Checkout

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,24 +1,40 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class CartPage(BasePage):
 
     CART_ITEMS = (By.CLASS_NAME, "cart_item")
     CHECKOUT_BTN = (By.ID, "checkout")
+    CART_CONTAINER = (By.ID, "cart_contents_container")  # wait for container
+
+    def wait_for_cart_loaded(self, timeout=10):
+        WebDriverWait(self.driver, timeout).until(
+            EC.visibility_of_element_located(self.CART_CONTAINER)
+        )
 
     def get_items_count(self):
+        self.wait_for_cart_loaded()
         return len(self.find_all(self.CART_ITEMS))
     
     def go_to_checkout(self):
+        self.wait_for_cart_loaded()
         self.click(self.CHECKOUT_BTN)
 
     def is_cart_empty(self):
+        self.wait_for_cart_loaded()
         return len(self.driver.find_elements(self.CART_ITEMS))
     
     def remove_all_products(self):
+        self.wait_for_cart_loaded()
         buttons = self.get_remove_buttons()
         for btn in buttons:
             btn.click()
 
     def is_loaded(self):
-        return "cart" in self.driver.current_url            
+        try:
+            self.wait_for_cart_loaded()
+            return True
+        except:
+            return False            
