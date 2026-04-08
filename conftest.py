4aa70c9764d0b4897848c246f0111bc14fd43941
Patryk Kwiatkowski <patrykkwiatkowski714@gmail.com>
2026-04-08 14:24:45 +0200

    CI/CD

--- conftest.py
+++ conftest.py
@@ -5,17 +5,19 @@ from datetime import datetime
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import prefs
+from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture
-def driver():
+def init_driver():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")  
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
 
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    service = Service(ChromeDriverManager().install())  # <--- używamy Service
+    driver = webdriver.Chrome(service=service, options=options)  # <--- podajemy service i options
     yield driver
     driver.quit()
 
