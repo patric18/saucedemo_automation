2c4f5b6b34f125da9a8be82e791eb61862ed4453
Patryk Kwiatkowski <patrykkwiatkowski714@gmail.com>
2026-04-09 12:28:24 +0200

    CI/CD doesnt work here without time sleep..

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -64,13 +64,13 @@ class CheckoutPage(BasePage):
             EC.presence_of_element_located(self.CONTINUE_BTN)
         )
 
-        # Poczekaj 0.2s na stabilizację inputów
-        time.sleep(1)
+        # Poczekaj 0.1s na stabilizację inputów
+        time.sleep(0.1)
 
         self.driver.execute_script(
             "arguments[0].scrollIntoView({block: 'center'});", continue_btn
         )
-        time.sleep(1)
+        time.sleep(0.1)
 
         self.driver.execute_script("arguments[0].click();", continue_btn)
 
