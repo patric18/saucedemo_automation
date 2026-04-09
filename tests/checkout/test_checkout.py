82c13f6928d8fadd21a36af6aa5c3d3b3f004fbc
Patryk Kwiatkowski <patrykkwiatkowski714@gmail.com>
2026-04-09 13:38:11 +0200

    Checkout - timesleep

--- tests/checkout/test_checkout.py
+++ tests/checkout/test_checkout.py
@@ -4,6 +4,7 @@ from pages.cart_page import CartPage
 from pages.checkout_page import CheckoutPage
 from utils.data import USER, PASSWORD, INVALID_CHECKOUT, VALID_CHECKOUT
 import pytest
+import time
 
 def test_checkout_success(driver):
     login = LoginPage(driver)
@@ -12,37 +13,40 @@ def test_checkout_success(driver):
     checkout = CheckoutPage(driver)
 
     login.open()
-    login.login(USER,PASSWORD)
+    login.login(USER, PASSWORD)
 
     inventory.add_products(1)
     inventory.go_to_cart()
     cart.go_to_checkout()
 
     checkout.fill_form(*VALID_CHECKOUT)
+    time.sleep(0.3)  # krótka pauza dla JS
     print("Checkout form values:", *VALID_CHECKOUT)
+    
     checkout.continue_checkout()
     checkout.finish()
 
     assert "THANK YOU" in checkout.get_success_message().upper()
 
+
 @pytest.mark.parametrize("firstname,lastname,postalcode,error", INVALID_CHECKOUT)
-def test_checkout_missing_data(driver,firstname,lastname,postalcode,error):
+def test_checkout_missing_data(driver, firstname, lastname, postalcode, error):
     login = LoginPage(driver)
     inventory = InventoryPage(driver)
     cart = CartPage(driver)
     checkout = CheckoutPage(driver)   
 
     login.open()
-    login.login(USER,PASSWORD)
+    login.login(USER, PASSWORD)
 
     inventory.add_products(1)
     inventory.go_to_cart()
     cart.go_to_checkout()
 
-    checkout.fill_form(firstname,lastname,postalcode)
+    checkout.fill_form(firstname, lastname, postalcode)
+    time.sleep(0.3)  # krótka pauza dla JS
     checkout.continue_checkout(wait_for_step_two=False)
 
-    assert checkout.get_error_message() == error
-
-    #error_message = checkout.get_error()
-    #assert error in error_message, f"Expected error '{error}' but got '{error_message}'"
+    actual_error = checkout.get_error_message()
+    print(f"Expected error: '{error}' | Actual error: '{actual_error}'")
+    assert actual_error == error
